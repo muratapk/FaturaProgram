6276b48caa6506cc5988dc1af3789f9b99e2da14
muratapk <muratapk@hotmail.com>
2024-11-28 21:24:35 +0300

    kodlar

--- FaturaProgram/Form1.cs
+++ FaturaProgram/Form1.cs
@@ -27,13 +27,17 @@ namespace FaturaProgram
             string sorgu = "insert into musteri(ad,vno,adres,tel) values (@ad,@vno,@adres,@tel)";
             try
             {
+                conn.Open();
                 SqlCommand cmd = new SqlCommand(sorgu, conn);
+                cmd.Parameters.Clear();
                 cmd.Parameters.AddWithValue("@ad", textBox1.Text);
                 cmd.Parameters.AddWithValue("@vno", textBox2.Text);
                 cmd.Parameters.AddWithValue("@adres",textBox3.Text);
                 cmd.Parameters.AddWithValue("@tel", textBox4.Text);
                 cmd.ExecuteNonQuery();
                 MessageBox.Show("Kayıt Eklenmiştir");
+                doldur();
+                conn.Close();
             }
             catch (Exception ex)
             {
@@ -41,9 +45,17 @@ namespace FaturaProgram
                 throw;
             }
         }
-
+       private  void doldur()
+        {
+            string cumle = "select * from musteri";
+            SqlDataAdapter adapter = new SqlDataAdapter(cumle, conn);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
+            groupBox1.Visible = false;
             try
             { 
 
@@ -60,6 +72,100 @@ namespace FaturaProgram
 
                 MessageBox.Show("Hata Oluştu" + ex.Message);
             }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int satir = dataGridView1.CurrentRow.Index;
+            textBox1.Text = dataGridView1.Rows[satir].Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[satir].Cells[2].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[satir].Cells[3].Value.ToString();
+            textBox4.Text = dataGridView1.Rows[satir].Cells[4].Value.ToString();
+            txtId.Text = dataGridView1.Rows[satir].Cells[0].Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                string cumle = "Update musteri set ad=@ad, vno=@vno,adres=@adres,tel=@tel where Id=@Id";
+                SqlCommand cmd = new SqlCommand(cumle, conn);
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@ad", textBox1.Text);
+                cmd.Parameters.AddWithValue("@vno", textBox2.Text);
+                cmd.Parameters.AddWithValue("@adres", textBox3.Text);
+                cmd.Parameters.AddWithValue("@tel", textBox4.Text);
+                cmd.Parameters.AddWithValue("@Id", txtId.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Kayıt Güncellendi");
+                doldur();
+                conn.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult cevap = MessageBox.Show("Bu Kaydı Silmek İstiyor musunuz", "Kayıt Silme", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (DialogResult.Yes == cevap) 
+            {
+                conn.Open();
+                string cumle = "Delete from musteri where Id=@Id";
+                SqlCommand cmd = new SqlCommand(cumle, conn);
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@Id", txtId.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Kayıt Silindi");
+                doldur();
+                conn.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true;
+        }
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            string sorgu = "Select * from musteri where ad like '%" + textBox5.Text +"%'";
+            SqlDataAdapter da=new SqlDataAdapter(sorgu, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void radioButton2_Click(object sender, EventArgs e)
+        {
+            string sorgu = "Select * from musteri where vno like '%" + textBox5.Text + "%'";
+            SqlDataAdapter da = new SqlDataAdapter(sorgu, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void radioButton3_Click(object sender, EventArgs e)
+        {
+            string sorgu = "Select * from musteri where adres like '%" + textBox5.Text + "%'";
+            SqlDataAdapter da = new SqlDataAdapter(sorgu, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+
+        private void radioButton4_Click(object sender, EventArgs e)
+        {
+            string sorgu = "Select * from musteri where tel like '%" + textBox5.Text + "%'";
+            SqlDataAdapter da = new SqlDataAdapter(sorgu, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
         }
     }
 }
